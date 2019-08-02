35fe8c07d7896cbae13646ac1c0352bb54a3a573
DRUM <borber.drum@gmail.com>
2019-08-03 00:55:40 +0800

    update from gridea: 2019-08-03 00:55:40

--- media/scripts/lib/music.js
+++ media/scripts/lib/music.js
@@ -1,3 +1,6 @@
+import 'https://cdnjs.cloudflare.com/ajax/libs/aplayer/1.10.1/APlayer.min.css';
+import APlayer from 'APlayer';
+
 const ap = new APlayer({
     container: document.getElementById('player'),
     mini: true,
