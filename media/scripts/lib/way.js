8eda1ec30dd5b7ceb0c03610df4a4043d5b97a0c
DRUM <borber.drum@gmail.com>
2019-08-22 19:45:47 +0800

    update from gridea: 2019-08-22 19:45:45

---
+++ media/scripts/lib/way.js
@@ -0,0 +1,2 @@
+//jQuery显隐菜单
+$(document).ready(function(){$('.previous').hover(function(){$(".prevspan").show();$('.prev').hide()},function(){$(".prevspan").hide();$('.prev').show()});$('.nextpost').hover(function(){$(".nextspan").show();$('.next').hide()},function(){$(".nextspan").hide();$('.next').show()})});
