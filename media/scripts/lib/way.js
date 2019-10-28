3ddded88c55ecb5b470465c6d0925c3346543d9c
BORBER <borber.drum@gmail.com>
2019-10-28 10:52:01 +0800

    update from gridea: 2019-10-28 10:52:00

--- media/scripts/lib/way.js
+++
@@ -1,2 +0,0 @@
-//jQuery显隐菜单
-$(document).ready(function(){$('.previous').hover(function(){$(".prevspan").show();$('.prev').hide()},function(){$(".prevspan").hide();$('.prev').show()});$('.nextpost').hover(function(){$(".nextspan").show();$('.next').hide()},function(){$(".nextspan").hide();$('.next').show()})});
